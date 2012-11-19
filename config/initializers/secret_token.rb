d0e924ac6a14f5d0223b4cb966fd14b29585ae18
Boris Babusenko <boris.bbk@gmail.com>
2012-11-19 20:58:37 +0200

    Inits project.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dnd4CharacterBuilder::Application.config.secret_token = 'efbe001ceb88f8b251cac43b5f7b44966e434ecf798b906bbe38ffd5369218d81124a1964a53345ae44e725b428c97d892db6d25df00d8bd45a4533668a3ec28'
