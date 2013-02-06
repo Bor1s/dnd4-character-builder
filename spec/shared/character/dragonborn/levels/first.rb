8cd0f0494b587e30676f820c381c5f6f6fb25000
Boris Babusenko <boris.babusenko@reevoo.com>
2013-02-06 13:28:17 +0000

    Fixes stupid tab indents.

--- spec/shared/character/dragonborn/levels/first.rb
+++ spec/shared/character/dragonborn/levels/first.rb
@@ -1,30 +1,30 @@
 shared_context "1st level dragonborn" do
-	context "1st level dragonborn (cleric)" do
-		subject do
+  context "1st level dragonborn (cleric)" do
+    subject do
       build_character(race: :dragonborn_character, level: 1)
-		end
+    end
 
     its(:vision) { should eq 'normal' }
     its(:speed) { should eq 6 }
     its(:size) { should eq 'medium' }
 
-		it "should have appropriate class" do
-			subject.character_race.name.should eq "Dragonborn"
-		end
+    it "should have appropriate class" do
+      subject.character_race.name.should eq "Dragonborn"
+    end
 
-		it "should have +2 Strength and +2 Charisma ability scores modifiers" do
-			subject.strength.should eq 18
-			subject.charisma.should eq 12
-		end
+    it "should have +2 Strength and +2 Charisma ability scores modifiers" do
+      subject.strength.should eq 18
+      subject.charisma.should eq 12
+    end
 
     it "should have 'Common' and 'Draconic' languages" do
       subject.languages.map(&:name).should eq ["Common", "Draconic"]
     end
 
-		it "should have +2 History and +2 Intimidate skills bonuses" do
-			subject.history.should eq 2
-			subject.intimidate.should eq 2
-		end
+    it "should have +2 History and +2 Intimidate skills bonuses" do
+      subject.history.should eq 2
+      subject.intimidate.should eq 2
+    end
 
     it "should have 'Draconic Heritage' bonus to healing_surge_value" do
       subject.healing_surge_value.should eq (subject.hit_points / 4 + subject.constitution_modifier)
@@ -35,5 +35,5 @@ shared_context "1st level dragonborn" do
     end
 
     include_context "hit points, healing surges and bloodied value"
-	end
+  end
 end
